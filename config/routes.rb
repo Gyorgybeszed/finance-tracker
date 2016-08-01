d31873bc3a91db9da78ad85b6fb760fbab6e609f
Gyuri <newsletter.of.dbg@gmail.com>
2016-08-01 22:16:12 +0000

    Add destroy Friendship and Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, :controllers => {:registrations => "user/registrations"} 
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show]
   resources :friendships
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
   post 'add_friend', to: 'users#add_friend'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
